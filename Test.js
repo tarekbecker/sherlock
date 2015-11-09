bf71697e24efca95b1bc56ff9f00563ea0856596
Tarek Auel <tarek.auel@gmail.com>
2015-11-10 00:26:49 +0100

    add array methods

---
+++ Test.js
@@ -0,0 +1,25 @@
+/*var a =  [];
+var b = [1, 2, 3];
+a.concat(b);
+
+(function() {
+    var a = [1, 2, 3];
+    a.push(1);
+    a.pop();
+    a.push(5);
+    a.reverse();
+    a.concat([9]);
+    a.sort();
+})();
+b[0] = 5;
+a.filter(function (x) {return x > 2});
+*/
+////
+
+var a = [];
+a.push(5);
+if (true) {
+    var x;
+    a.pop();
+}
+a.push(10);
--- sherlock.js
+++ sherlock.js
@@ -4,80 +4,112 @@
 
 (function (sandbox) {
 
-    function ArrayReference(base, name) {
-        var optVer = JSON.parse(JSON.stringify(base));
-        var allFree = true;
-        var locked = false;
-        var lockedValues = {};
-        var references = {};
-
-        this.push = function(val) {
-          if (!locked && !lockedValues[optVer.length + 1]) {
-              optVer.push(val);
-          }
-        };
-
-        this.pop = function() {
-            if (!locked && !lockedValues[optVer.length - 1]) {
-                optVer.pop();
-            }
-        };
-
-        this.update = function(offset, val) {
-            if (!locked && !lockedValues[offset]) {
-                optVer[offset] = val;
-            }
-        };
-
-        this.shift = function() {
-            if (!locked && allFree) {
-                optVer.shift();
-            }
-        };
-
-        this.unshift = function() {
-            if (!locked && allFree) {
-                optVer.unshift();
-            }
-        };
-
-        this.addRef = function(name) {
-            references[name] = true;
-        };
-
-        this.equals = function(val) {
-            return base === val;
-        };
-
-        this.lock = function(num) {
-            if (num >= 0) {
-                lockedValues[num] = true;
-                allFree = false;
-            } else {
-                locked = true;
-                allFree = false;
-            }
-        };
-
-        this.print = function() {
-            console.log("references " + JSON.stringify(references));
-            console.log("opt version " + JSON.stringify(optVer));
-            console.log("allFree " + JSON.stringify(allFree));
-            console.log("locked " + JSON.stringify(locked));
-            console.log("locked values " + JSON.stringify(lockedValues));
-            console.log();
-        };
-
-        this.addRef(name);
-    }
-
     function MyAnalysis () {
-        var iidToLocation = sandbox.iidToLocation;
-        var Constants = sandbox.Constants;
+        var iidToLocation = function(iid) {
+            var loc = sandbox.iidToLocation(J$.getGlobalIID(iid)).split(":").slice(-4, -2);
+            return "row: " + loc[0];
+        };
+
+        function ArrayReference(base, name, iid) {
+            var optVer = JSON.parse(JSON.stringify(base));
+            var isOpt = false;
+            var locked = false;
+            var lockedValues = {};
+            var references = [];
+
+            var allFree = function() {
+                var value;
+                for(value in lockedValues) {
+                    if(lockedValues.hasOwnProperty(value) && lockedValues[value]) {
+                        return false;
+                    }
+                }
+                return true;
+            };
+
+            this.concat = function(args) {
+                if (!locked) {
+                    var i = 0;
+                    while(args.hasOwnProperty(i + "") && args[i] instanceof Array) {
+                        optVer = optVer.concat(args[i]);
+                        i++;
+                    }
+                    isOpt = true;
+                }
+            };
+
+            this.callOnUnlocked = function(f, args) {
+                if (!locked && allFree()) {
+                    f.apply(optVer, args);
+                    isOpt = true;
+                } else {
+                    locked = true;
+                }
+            };
+
+            this.push = function(val) {
+                if (!locked && !lockedValues[optVer.length + 1]) {
+                    optVer.push(val);
+                    isOpt = true;
+                }
+            };
+
+            this.pop = function() {
+                if (!locked && !lockedValues[optVer.length - 1]) {
+                    optVer.pop();
+                    isOpt = true;
+                }
+            };
+
+            this.update = function(offset, val) {
+                if (!locked && !lockedValues[offset]) {
+                    optVer[offset] = val;
+                    isOpt = true;
+                }
+            };
+
+            this.addRef = function(name, iid) {
+                references.push({
+                    name: name,
+                    loc: iidToLocation(iid)
+                });
+            };
+
+            this.equals = function(val) {
+                return base === val;
+            };
+
+            this.lock = function(num) {
+                if (num >= 0) {
+                    lockedValues[num] = true;
+                } else {
+                    locked = true;
+                }
+            };
+
+            this.debug = function() {
+                console.log("references " + JSON.stringify(references));
+                console.log("is opt " + JSON.stringify(isOpt));
+                console.log("opt version " + JSON.stringify(optVer));
+                console.log("allFree " + JSON.stringify(allFree));
+                console.log("locked " + JSON.stringify(locked));
+                console.log("locked values " + JSON.stringify(lockedValues));
+                console.log();
+            };
+
+            this.get = function() {
+                return {
+                    isOptimized: isOpt,
+                    optimizedVersion: optVer,
+                    references: references
+                }
+            };
+
+
+            this.addRef(name, iid);
+        }
 
         var initArrays = [];
-        var stack = [];
-        var finished = [];
 
         var getRef = function(base) {
             for(var i = 0; i < initArrays.length; i++) {
@@ -85,16 +117,6 @@
                     return initArrays[i];
                 }
             }
-        };
-
-        this.functionEnter = function(iid, f, dis, args) {
-            stack.push(initArrays);
-            initArrays = [];
-        };
-
-        this.functionExit = function(iid, returnVal, wrappedExceptionVal) {
-            finished.push(initArrays);
-            initArrays = stack.pop();
         };
 
         this.putFieldPre = function(iid, base, offset, val, isComputed, isOpAssign) {
@@ -112,9 +134,9 @@
             if (val instanceof Array) {
                 var ref;
                 if ((ref = getRef(val))) {
-                    ref.addRef(name);
+                    ref.addRef(name, iid);
                 } else {
-                    initArrays.push(new ArrayReference(val, name));
+                    initArrays.push(new ArrayReference(val, name, iid));
                 }
             }
         };
@@ -133,8 +155,34 @@
         };
 
         this.invokeFunPre = function(iid, f, base, args, result, isConstructor, isMethod, functionIid) {
+            // Methods according to http://www.ecma-international.org/ecma-262/5.1/#sec-15.4.4
             var ref;
-            if (f === Array.prototype.push) {
+            // TODO: slice may only lock used values
+            var methodsLockArray = [
+                Array.prototype.toString, Array.prototype.toLocaleString, Array.prototype.indexOf,
+                Array.prototype.lastIndexOf, Array.prototype.every, Array.prototype.some, Array.prototype.forEach,
+                Array.prototype.map, Array.prototype.reduce, Array.prototype.reduceRight, Array.prototype.join,
+                Array.prototype.filter, Array.prototype.slice
+            ];
+
+            var callOnUnlocked = [
+                Array.prototype.reverse, Array.prototype.shift, Array.prototype.sort, Array.prototype.splice,
+                Array.prototype.unshift
+            ];
+
+            if (callOnUnlocked.indexOf(f) !== -1) {
+                if ((ref = getRef(base))) {
+                    ref.callOnUnlocked(f, args);
+                }
+            } else if (methodsLockArray.indexOf(f) !== -1) {
+                if ((ref = getRef(base))) {
+                    ref.lock();
+                }
+            } else if (f == Array.prototype.concat) {
+                if ((ref = getRef(base))) {
+                    ref.concat(args)
+                }
+            } else if (f === Array.prototype.push) {
                 if ((ref = getRef(base))) {
                     ref.push(args["0"]);
                 }
@@ -142,29 +190,19 @@
                 if ((ref = getRef(base))) {
                     ref.pop();
                 }
-            } else if (f === Array.prototype.shift) {
-                if ((ref = getRef(base))) {
-                    ref.shift();
-                }
-            } else if (f === Array.prototype.unshift) {
-                if ((ref = getRef(base))) {
-                    ref.unshift();
-                }
             }
         };
 
         this.endExecution = function() {
             for(var i = 0; i < initArrays.length; i++) {
-                initArrays[i].print();
+                initArrays[i].debug();
             }
-            console.log("-------------------");
-            console.log("-------------------");
-            for (i = 0; i < finished.length; i++) {
-                for (var j=0; j < finished[i].length; j++) {
-                    finished[i][j].print();
-                }
-                console.log("----------------");
+
+            var out = [];
+            for(i = 0; i < initArrays.length; i++) {
+                out.push(initArrays[i].get());
             }
+            console.log(JSON.stringify(out));
         }
     }
 
